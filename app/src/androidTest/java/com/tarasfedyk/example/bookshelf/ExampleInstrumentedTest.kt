3f6515b8d5c29e877b9b1953f5b8e4cab085d9e9
Taras Fedyk <taras.i.fedyk@gmail.com>
2020-10-26 18:20:07 +0200

    Fix the configuration for automated testing

--- app/src/androidTest/java/com/tarasfedyk/example/bookshelf/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/tarasfedyk/example/bookshelf/ExampleInstrumentedTest.kt
@@ -1,10 +1,8 @@
 package com.tarasfedyk.example.bookshelf
 
 import androidx.test.platform.app.InstrumentationRegistry
-import androidx.test.ext.junit.runners.AndroidJUnit4
 
 import org.junit.Test
-import org.junit.runner.RunWith
 
 import org.junit.Assert.*
 
@@ -13,8 +11,8 @@ import org.junit.Assert.*
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
-@RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+
     @Test
     fun useAppContext() {
         // Context of the app under test.
