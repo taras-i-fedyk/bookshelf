eefccfb2a6db7a9713da358cd2bd6f98d04833bb
Taras Fedyk <taras.i.fedyk@gmail.com>
2020-11-19 17:12:13 +0200

    Convert a property to a function

--- app/src/main/java/com/tarasfedyk/example/bookshelf/biz/utils/Utils.kt
+++ app/src/main/java/com/tarasfedyk/example/bookshelf/biz/utils/Utils.kt
@@ -2,6 +2,6 @@ package com.tarasfedyk.example.bookshelf.biz.utils
 
 import android.os.Looper
 
-val isMainThread: Boolean = Thread.currentThread() == Looper.getMainLooper().thread
+fun isMainThread(): Boolean = Thread.currentThread() == Looper.getMainLooper().thread
 
-fun enforceMainThread() = check(isMainThread) { "The current thread is not the main thread" }
+fun enforceMainThread() = check(isMainThread()) { "The current thread is not the main thread" }
